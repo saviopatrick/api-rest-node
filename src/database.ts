188690c625f4371d005214ecff46da32c3cb84d1
saviopatrick <saviopatrick09@gmail.com>
2024-11-23 20:16:13 -0300

    chore: add initial project files and configurations

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { knex as setupKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: './db/migrations',
+    extension: 'ts',
+  },
+}
+
+export const knex = setupKnex(config)
