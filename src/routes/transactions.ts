f4987a2b1ca4fff0efffad3faf19a864f70ed650
saviopatrick <saviopatrick09@gmail.com>
2024-11-23 23:42:52 -0300

    chore: add GET routes for transactions and summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,27 @@ import { knex } from '../database'
 import crypto from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTrasactionsShema = z.object({
+      id: z.string(),
+    })
+    const { id } = getTrasactionsShema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+    return { summary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionSchema = z.object({
       title: z.string(),
